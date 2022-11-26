c4a21e489fa7157002b3dc17c3d4132c87399559
fawad <fawadstar6@gmail.com>
2022-11-26 23:13:02 +0500

    updates

--- Weather_Forecast/settings.py
+++ Weather_Forecast/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
 
 # Default primary key field type
